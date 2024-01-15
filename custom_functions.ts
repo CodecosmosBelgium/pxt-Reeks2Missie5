095bf03ca34a4724b399c0633d9fba6a2255e1ff
SebastiaanMoons <sebastiaan.moons@hotmail.com>
2024-01-15 13:05:43 +0100

    Update custom_functions.ts, t_extra.md, t_extra2.md

--- custom_functions.ts
+++ custom_functions.ts
@@ -28,7 +28,7 @@ namespace AgentExtension {
         agent.move(FORWARD, 1)
     }
     
-    //% block="agent turns 360 degrees"
+    //% block="agent turn 360 degrees"
     export function agent360_ex2() {
         if (!blocks.testForBlock(FIRE_CORAL_BLOCK, blockBelowAgent) && !blocks.testForBlock(HORN_CORAL_BLOCK, blockBelowAgent)) {
             for (let i = 0; i < 4; i++) {
@@ -41,7 +41,7 @@ namespace AgentExtension {
         player.execute(`scoreboard players set @a level_timer 0`)
     }
 
-    //% block="agent turns 360 degrees"
+    //% block="agent turn 360 degrees"
     export function agent360_ex4() {
         if (!blocks.testForBlock(RED_TERRACOTTA, blockBelowAgent) && !blocks.testForBlock(SAND, blockBelowAgent)) {
             for (let i = 0; i < 4; i++) {
@@ -53,10 +53,32 @@ namespace AgentExtension {
         }
         player.execute(`scoreboard players set @a level_timer 0`)
     }
+    
+    //% block="agent turn left"
+    export function agent_turn_left() {
+        if (blocks.testForBlock(GOLD_BLOCK, blockBelowAgent)) {
+            agent.turn(LEFT_TURN)
+            player.execute(`scoreboard players add @a correctBlocks 1`)
+        } else {
+            wrong()
+        }
+        player.execute(`scoreboard players set @a level_timer 0`)
+    }
+    
+    //% block="agent turn right"
+    export function agent_turn_right() {
+        if (blocks.testForBlock(IRON_BLOCK, blockBelowAgent)) {
+            agent.turn(RIGHT_TURN)
+            player.execute(`scoreboard players add @a correctBlocks 1`)
+        } else {
+            wrong()
+        }
+        player.execute(`scoreboard players set @a level_timer 0`)
+    }
 }
 
 
-//% color=190 weight=100 icon="\uf20a" block="CodeCosmos"
+//% color="#5B99A5" weight=100 icon="\uf20a" block="CodeCosmos"
 namespace CodeCosmos {
     //% block="is block %block=block below agent"
     //% block.shadow=minecraftBlock
@@ -127,9 +149,6 @@ namespace CodeCosmos {
         }
     }
 
-    player.say("kek")
-
-
     //% block="spawn dolphin"
     export function spawnDolphin() {
         if (blocks.testForBlock(RED_TERRACOTTA, blockBelowAgent)) {
@@ -143,6 +162,26 @@ namespace CodeCosmos {
     export function spawnAxolotl() {
         if (!blocks.testForBlock(RED_TERRACOTTA, blockBelowAgent) && !blocks.testForBlock(SAND, blockBelowAgent)) {
             player.execute(`execute @v ~ ~1 ~ summon axolotl`)
+            player.execute(`scoreboard players add @a correctBlocks 1`)
+        } else {
+            wrong()
+        }
+    }
+
+    //% block="place purple coral"
+    export function placeCoralPurple() {
+        if (blocks.testForBlock(BUBBLE_CORAL_BLOCK, blockBelowAgent)) {
+            player.execute(`execute @v ~ ~ ~ setblock ~ ~ ~ bubble_coral`)
+            player.execute(`scoreboard players add @a correctBlocks 1`)
+        } else {
+            wrong()
+        }
+    }
+    
+    //% block="spawn turtle"
+    export function spawnTurtle() {
+        if (!blocks.testForBlock(GOLD_BLOCK, blockBelowAgent) && !blocks.testForBlock(IRON_BLOCK, blockBelowAgent)) {
+            player.execute(`execute @v ~ ~1 ~ summon turtle`)
             player.execute(`scoreboard players add @a correctBlocks 1`)
         } else {
             wrong()
--- t_extra.md
+++ t_extra.md
@@ -4,7 +4,18 @@
 
 ```blocks
 player.onChat("extraLevel1", function () {
-    
+    for (let index = 0; index < 12; index++) {
+        AgentExtension.agentMoveForward()
+        if (CodeCosmos.testForBlock_belowAgent(BUBBLE_CORAL_BLOCK)) {
+            CodeCosmos.placeCoralPurple()
+        } else if (CodeCosmos.testForBlock_belowAgent(GOLD_BLOCK)) {
+            AgentExtension.agent_turn_left()
+        } else if (CodeCosmos.testForBlock_belowAgent(IRON_BLOCK)) {
+            AgentExtension.agent_turn_right()
+        } else {
+            CodeCosmos.spawnTurtle()
+        }
+    }
 })
 ```
 
